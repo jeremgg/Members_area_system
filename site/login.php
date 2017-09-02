e82dac10282410da9fdb0b0cef6516fa70790c82
jeremgg <Votre@email.com>
2017-09-02 15:45:38 +0200

    add error message when all fields in the form are not filled

--- site/login.php
+++ site/login.php
@@ -15,38 +15,45 @@
 
 
     //verify that the user is trying to connect and that no form fields are empty
-    if(!empty($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
-        ///Save the user according to the email or pseudo entered in the form
-        //and that the user has a validated account
-        require_once 'inc/db.php';
-        $req = $pdo->prepare('SELECT * FROM users WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL');
-        $req->execute(['username' => $_POST['username']]);
-        $user = $req->fetch();
+    if(!empty($_POST)){
+        if(!empty($_POST['username']) && !empty($_POST['password'])){
+            ///Save the user according to the email or pseudo entered in the form
+            //and that the user has a validated account
+            require_once 'inc/db.php';
+            $req = $pdo->prepare('SELECT * FROM users WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL');
+            $req->execute(['username' => $_POST['username']]);
+            $user = $req->fetch();
 
 
-        //If the password entered by the user matches the password entered in the database
-        //redirect the user to his personal page
-        if(password_verify($_POST['password'], $user->password)){
-            $_SESSION['auth'] = $user;
-            $_SESSION['flash']['success'] = "Bienvenue, vous êtes connecté à votre compte";
+            //If the password entered by the user matches the password entered in the database
+            //redirect the user to his personal page
+            if(password_verify($_POST['password'], $user->password)){
+                $_SESSION['auth'] = $user;
+                $_SESSION['flash']['success'] = "Bienvenue, vous êtes connecté à votre compte";
 
-            //If the user checks the checkbox remember
-            if($_POST['remember']){
-                $remember_token = str_random(255);
-                $req = $pdo->prepare('UPDATE users SET remember_token = ? WHERE id = ?');
-                $req->execute([$remember_token, $user->id]);
+                //If the user checks the checkbox remember
+                if($_POST['remember']){
+                    $remember_token = str_random(255);
+                    $req = $pdo->prepare('UPDATE users SET remember_token = ? WHERE id = ?');
+                    $req->execute([$remember_token, $user->id]);
 
-                //The result of the request is saved in a relatively complex cookie
-                setcookie('remember', $user->id . '==' . $remember_token . sha1($user->id . 'phone'), time() + 60 * 60 * 24 * 7);
+                    //The result of the request is saved in a relatively complex cookie
+                    setcookie('remember', $user->id . '==' . $remember_token . sha1($user->id . 'phone'), time() + 60 * 60 * 24 * 7);
+                }
+
+                header('location: account.php');
+                exit();
             }
-
-            header('location: account.php');
-            exit();
+            else{
+                $_SESSION['flash']['danger'] = "Identifiant ou mot de passe incorrect";
+            }
         }
+
         else{
-            $_SESSION['flash']['danger'] = "Identifiant ou mot de passe incorrect";
+            $_SESSION['flash']['danger'] = "Tous les champs doivent être renseignés";
         }
     }
+
 ?>
 
 
