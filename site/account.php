a7661e2c8859f818463ea9c41cf76cb299cbecf2
jeremgg <Votre@email.com>
2017-09-02 12:29:51 +0200

    allow the user to change his password on his personal page

--- site/account.php
+++ site/account.php
@@ -7,6 +7,28 @@
 
     //If the user is not logged in, he does not have access to his personal page
     logged_only();
+
+
+    //Verify that the password change form sent data
+    //And that the password fields have the same value
+    if(!empty($_POST)){
+        if(empty($_POST['password']) || $_POST['password'] != $_POST['password-confirm']){
+            $_SESSION['flash']['danger'] = "Les mots de passes ne correspondent pas";
+        }
+        else{
+            $user_id = $_SESSION['auth']->id;
+
+            //Encrypt the user's password
+            $password= password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+            //Update the password in the database
+            require_once 'inc/db.php';
+            $req = $pdo->prepare('UPDATE users SET password = ? WHERE id = ?');
+            $req->execute([$password, $user_id]);
+
+            $_SESSION['flash']['success'] = "Votre mot de passe a été changé avec succès";
+        }
+    }
 ?>
 
 
